04e360f478c001522e87b20d332680b275e5c281
rotht2 <rotht2@nku.edu>
2017-03-19 11:30:59 -0400

    default die to 6 faces; add javadocs

--- src/game/pieces/Die.groovy
+++ src/game/pieces/Die.groovy
@@ -4,11 +4,20 @@ class Die {
 
     def faces
     private static Random rand
-    Die(faces){
+
+    /**
+     * Die class makes a new playing die and allows you to roll and get a random
+     * generated number between 1 and the faces of the die
+     * @param faces is the number of faces on the die you wish to construct
+     */
+    Die(faces = 6){
         this.faces = faces
         rand = new Random(System.currentTimeMillis())
     }
 
+    /**
+     * Rolls your die and returns the result
+     */
     static roll(){
         def randomNumber = (rand.nextInt() % 6)
         def positiveNumber = randomNumber > 0 ? randomNumber : randomNumber * -1
