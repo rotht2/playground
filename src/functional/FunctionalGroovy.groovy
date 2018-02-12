30a50fe7b060cfd9d90bb3745b7ee93be66d54fd
Tyler <tyler.roth09@gmail.com>
2018-02-12 18:59:12 -0500

    basic lambdas first sort by number of keys in the shortcut

--- src/functional/FunctionalGroovy.groovy
+++ src/functional/FunctionalGroovy.groovy
@@ -1,4 +1,21 @@
 package functional
 
 class FunctionalGroovy {
+
+    // Filter = findAll
+    // map = collect
+    // fold = inject
+
+    def square = { number -> number * number}
+
+    def squareIt = {it * it}
+
+    def add = {x, y -> x + y}
+
+    def addWithTypes = {int x, int y -> x + y}
+
+    def sortByNumberOfKeys(List<Shortcut> shortcutList){
+        println(shortcutList.collect{ it.keys.size() }.sort())
+    }
+
 }
--- test/functional/FunctionalGroovyTest.groovy
+++ test/functional/FunctionalGroovyTest.groovy
@@ -1,13 +1,17 @@
 package functional
 
 import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
 
-class FunctionalGroovyTest extends GroovyTestCase {
+class FunctionalGroovyTest {
 
     List shortCuts
+    FunctionalGroovy functionalGroovy
 
     @Before
     void setup(){
+        functionalGroovy = new FunctionalGroovy()
         shortCuts = new ArrayList()
         shortCuts.add(new Shortcut('copy','control', 'c'))
         shortCuts.add(new Shortcut('paste','control', 'v'))
@@ -16,4 +20,36 @@ class FunctionalGroovyTest extends GroovyTestCase {
         shortCuts.add(new Shortcut('duplicate line in intellij','control', 'd'))
         shortCuts.add(new Shortcut('convert Java file to Kotlin','control', 'shift', 'alt', 'k'))
     }
+
+    @Test
+    void squaresTwoNumbers(){
+        assert functionalGroovy.square(4) == 16
+        assert functionalGroovy.squareIt(4) == 16
+    }
+
+    @Ignore("Fails because there is no * operator for string")
+    @Test
+    void takesTheSquareRootOfTwoNonNumbers(){
+        assert functionalGroovy.square("A") == 16
+        assert functionalGroovy.squareIt("a") == 16
+    }
+
+    @Test
+    void addsStuff(){
+        assert functionalGroovy.add(1,2) == 3
+        assert functionalGroovy.add("x", "y") == "xy"
+    }
+
+    @Test
+    void addWithTypes(){
+        assert functionalGroovy.addWithTypes(1,2) == 3
+        // Uncomment to see the types not match up.. Because I provided types in the lambda declaration
+//        assert functionalGroovy.addWithTypes("x", "y") == "xy"
+    }
+
+    @Test
+    void sortStuff(){
+        functionalGroovy.sortByNumberOfKeys(shortCuts)
+    }
+
 }
