70e29519eef3b65896f72864336ed7fa9566bf69
Tyler <tyler.roth09@gmail.com>
2018-02-18 20:24:13 -0500

    more functional elements

--- src/functional/FunctionalGroovy.groovy
+++ src/functional/FunctionalGroovy.groovy
@@ -2,6 +2,12 @@ package functional
 
 class FunctionalGroovy {
 
+    // TODO Metaclass out findAll, collect, and inject to rename them to Fiter map fold
+    /*
+        filter :
+        map :
+        fold :
+     */
     // Filter = findAll
     // map = collect
     // fold = inject
@@ -15,7 +21,29 @@ class FunctionalGroovy {
     def addWithTypes = {int x, int y -> x + y}
 
     def sortByNumberOfKeys(List<Shortcut> shortcutList){
+        // This sorts a collection of things returned by it.keys.size. In our example this is the number of keys in the shortcut
         println(shortcutList.collect{ it.keys.size() }.sort())
+
+        // This sorts the actual shortcut objects by the number of keys in the short cut.
+        println(shortcutList.sort({ it.keys.size() }))
     }
 
+    List<Shortcut> findShortCutContainingKey(List<Shortcut> shortcutList, String key) {
+        shortcutList.findAll({ it.keys.contains(key)})
+    }
+
+    //TODO inject
+    void injectShortCut(Shortcut shortcut){
+
+    }
+
+    // TODO pass closure to method
+    void methodWithClosureParam(Closure closure){
+        closure()
+    }
+
+    // TODO call the meta classed methods ( with the common functional names )
+    void sameExamplesAsAboveWithConventionalFuntionalNames(List<Shortcut> shortcuts){
+
+    }
 }
--- test/functional/FunctionalGroovyTest.groovy
+++ test/functional/FunctionalGroovyTest.groovy
@@ -13,12 +13,12 @@ class FunctionalGroovyTest {
     void setup(){
         functionalGroovy = new FunctionalGroovy()
         shortCuts = new ArrayList()
+        shortCuts.add(new Shortcut('convert Java file to Kotlin','control', 'shift', 'alt', 'k'))
         shortCuts.add(new Shortcut('copy','control', 'c'))
         shortCuts.add(new Shortcut('paste','control', 'v'))
         shortCuts.add(new Shortcut('cut','control', 'x'))
         shortCuts.add(new Shortcut('save','control', 's'))
         shortCuts.add(new Shortcut('duplicate line in intellij','control', 'd'))
-        shortCuts.add(new Shortcut('convert Java file to Kotlin','control', 'shift', 'alt', 'k'))
     }
 
     @Test
@@ -51,5 +51,18 @@ class FunctionalGroovyTest {
     void sortStuff(){
         functionalGroovy.sortByNumberOfKeys(shortCuts)
     }
+
+    @Test
+    void findstuff(){
+        println(functionalGroovy.findShortCutContainingKey(shortCuts, "c"))
+    }
+
+    @Test
+    void injectStuff(){
+//        shortCuts.inject()
+        println(shortCuts)
+    }
+
+
 
 }
